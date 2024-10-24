39567f3d18b93e6092197467dbbb4a4584674f4a
chaoowen <chanffe@gmail.com>
2024-10-24 18:35:28 +0800

    feat: update text

--- components/Page/Skills/SkillsDevelop.vue
+++ components/Page/Skills/SkillsDevelop.vue
@@ -1,3 +1,14 @@
 <template>
-  <p>hahaha</p>
+  <div class="space-y-4 text-primary">
+    <p>
+      目前使用 nuxt3 + Tailwindcss + vite 進行響應式網站開發，並使用 flowbite
+      建造共用元件模板，提升開發速度。
+    </p>
+    <p>
+      在程式協作面上，我熟悉 Git
+      版本控制，能夠有效地協作開發；使用者體驗面上，我與設計團隊溝通協作，將
+      figma 畫面變現實，並針對流程提出優化建議；串接 API
+      面上，我與後端工程師、DevOps 緊密合作，確保網站可以正確運行。
+    </p>
+  </div>
 </template>
