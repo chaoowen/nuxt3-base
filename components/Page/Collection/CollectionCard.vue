c0f16bf7fde97554c26cf272fa9d0c82c97b71bd
chaoowen <chanffe@gmail.com>
2024-10-25 14:15:00 +0800

    feat: update layout

--- components/Page/Collection/CollectionCard.vue
+++ components/Page/Collection/CollectionCard.vue
@@ -1,7 +1,8 @@
 <template>
   <div class="relative">
     <div
-      class="w-[92%] sm:w-[94%] lg:w-[95%] xl:w-[97%] min-h-20 ml-auto p-6 pl-10 bg-secondary-lighter rounded-lg card-shadow"
+      class="w-[92%] sm:w-[94%] lg:w-[95%] xl:w-[97%] min-h-20 ml-auto p-6 pl-10 rounded-lg card-shadow"
+      :class="link ? 'bg-secondary-lighter' : 'bg-gray-lightest'"
     >
       <div class="space-y-2 ubuntu-medium">
         <p class="text-primary">{{ title }}</p>
@@ -25,14 +26,19 @@
         </a>
       </div>
     </div>
-    <div class="leaf-icon w-12 h-12 md:w-16 md:h-16">
-      <img class="w-5" src="~/assets/images/leaf-2.png" alt="leaf-icon" />
+    <div
+      class="leaf-icon w-12 h-12 md:w-16 md:h-16"
+      :class="link ? 'bg-secondary' : 'bg-gray-lighter'"
+    >
+      <img class="w-5" :src="leafIcon" alt="leaf-icon" />
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
   import ArrowIcon from '~/assets/icons/arrow.svg?raw'
+  import Leaf from '~/assets/images/leaf-1.png'
+  import LeafWithLink from '~/assets/images/leaf-2.png'
 
   interface Props {
     title: string
@@ -42,13 +48,15 @@
     list?: string[]
   }
 
-  defineProps<Props>()
+  const props = defineProps<Props>()
+
+  const leafIcon = computed(() => (!!props.link ? LeafWithLink : Leaf))
 </script>
 
 <style scoped>
   .leaf-icon {
     @apply absolute left-0 top-1/2 -translate-y-1/2 z-10;
-    @apply flex-center bg-secondary rounded-full;
+    @apply flex-center rounded-full;
   }
 
   .card-shadow {
@@ -56,7 +64,7 @@
     box-shadow: 2px 2px 8px rgba(194 187 147 / 30%);
 
     &:hover {
-      @apply shadow-none border-secondary-light;
+      box-shadow: 4px 4px 12px rgba(194 187 147 / 30%);
     }
   }
 </style>
