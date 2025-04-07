9056a4c90eadd4d0971be83ae34be36c06fdf446
chaoowen <chanffe@gmail.com>
2025-04-07 23:02:32 +0800

    feat: hidden CoverImage in small size

--- components/Page/CoverImage.vue
+++ components/Page/CoverImage.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="relative parallax overflow-hidden">
+  <div class="hidden md:flex relative parallax overflow-hidden">
     <div class="w-full h-70 md:h-120 xl:h-160 overflow-hidden">
       <img
         class="parallaxImage w-full h-[110%] object-cover"
